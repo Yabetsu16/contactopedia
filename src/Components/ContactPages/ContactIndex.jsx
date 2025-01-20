3b2be66b636d5a1f79fd2c073d4a9c05217415b1
Yabetsu <it.jabez16@gmail.com>
2025-01-20 16:28:17 +0800

    Finish Individual Contact UI

---
+++ src/Components/ContactPages/ContactIndex.jsx
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import Header from "../Layout/Header";
+import AddRandomContact from "./AddRandomContact";
+import RemoveAllContact from "./RemoveAllContact";
+import AddContact from "./AddContact";
+import FavoriteContacts from "./FavoriteContacts";
+import GeneralContacts from "./GeneralContacts";
+import Footer from "../Layout/Footer";
+
+export class ContactIndex extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      contactList: [
+        {
+          id: 1,
+          name: "Ben Parker",
+          phone: "123-456-7770",
+          email: "ben@dotnetmastery.com",
+          isFavorite: false,
+        },
+        {
+          id: 2,
+          name: "Kathy Patrick",
+          phone: "111-222-0000",
+          email: "kathy@dotnetmastery.com",
+          isFavorite: true,
+        },
+        {
+          id: 3,
+          name: "Paul Show",
+          phone: "999-222-1111",
+          email: "paul@dotnetmastery.com",
+          isFavorite: true,
+        },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="container" style={{ minHeight: "85vh" }}>
+          <div className="row py-3">
+            <div className="col-4 offset-2">
+              <AddRandomContact />
+            </div>
+            <div className="col-4">
+              <RemoveAllContact />
+            </div>
+            <div className="row py-2">
+              <AddContact />
+            </div>
+            <div className="row py-2">
+              <FavoriteContacts
+                contacts={this.state.contactList.filter(
+                  (u) => u.isFavorite == true
+                )}
+              />
+            </div>
+            <div className="row py-2">
+              <GeneralContacts
+                contacts={this.state.contactList.filter(
+                  (u) => u.isFavorite == false
+                )}
+              />
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default ContactIndex;
